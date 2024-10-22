454abd7fd5ff638eb0ef92090d1bbca53b838960
Fernanda <eygleen.avila@alu.ing.unlp.edu.ar>
2024-10-22 17:39:56 -0300

    listar

--- src/ActivoDaoImpl.java
+++ src/ActivoDaoImpl.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class ActivoDaoImpl implements ActivoDao {
 
     // falta: verificar que tipo de activo es -- verificar si el activo ya esta en
     // la tabla y sumar -- verificar si la nomenclatura es un activo verdadero
+    // instans of
     public void GenerarActivo(String nomenclatura, double cantidad) {
         Connection c = null;
         Statement stmt = null;
@@ -47,9 +49,43 @@ public class ActivoDaoImpl implements ActivoDao {
     }
 
     @Override
-    public List<Activo> listarActivos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarActivos'");
-    }
+    /*
+     * Listar Mis Activos
+     * Muestra en pantalla información de los activos disponibles.
+     * Si bien los Activos se mostrarán ordenados por cantidad de manera
+     * descendente, debe ser capaz de ordenarse por nomenclatura de la moneda.
+     * Se espera que use alguno de los mecanismos de interfaz vistos en teoría
+     */
 
+    public List<Activo> listarActivos() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:BilleteraVirtual.db");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ACTIVO_CRIPTO;");
+
+            while (rs.next()) {
+                String nom = rs.getString("NOMENCLATURA");
+                double cant = rs.getDouble("CANTIDAD");
+
+                System.out.println("nom = " + nom);
+                System.out.println("cant = " + cant);
+
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+        return null;
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,8 @@
 public class Menu {
     public static void main(String[] args) {
         ActivoDao act = new ActivoDaoImpl();
-        act.GenerarActivo("ETdH", 5);
+        act.GenerarActivo("DOGE", 8);
+        act.listarActivos();
 
     }
 }
